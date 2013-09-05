2fb31f659aed4b729ee9679f54784a1a6d0a96ed
Paul Barton <pablo.barton@gmail.com>
2013-09-05 16:30:41 -0400

    Added some more max and min as commandline arguments to REVP

--- Rosalind/String_Algorithms/REVP.py
+++ Rosalind/String_Algorithms/REVP.py
@@ -4,11 +4,15 @@
 """Locating Restriction Sites
 
 Usage:
-  DNA.py <input> [--compare]
+  DNA.py <input> [--compare] [--max=MAX] [--min=MIN]
   DNA.py (--help | --version)
 
 Options:
   --compare       run a speed comparison of various methods
+  --max=MAX       Set the maximum length of palindrome to search for, even
+                  numbers should be used
+  --min=MIN       Set the minimum length of palindrome to search for, even
+                  numbers should be used. Less than 4 is not recommended.
   -h --help       show this help message and exit
   -v --version    show version and exit
 """
@@ -56,17 +60,17 @@ def parse_fasta_sequence(inp_file):
     return name, sequence
 
 
-def palindromes_by_nuclei(sequence, min_pal=4, max_pal=12):
+def palindromes_by_nuclei(sequence, max_pal=12, min_pal=4):
     '''
     Checks for reverse palindromes in a DNA sequence; acts as a generator that
     will yield the starting offset of a palindrome along with its length.
     '''
-    comp_dict = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    comp = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     for offset in range(len(sequence)):
         mod = 0
         #Length is twice the mod value
         try:
-            while sequence[offset - mod] == comp_dict[sequence[offset + mod + 1]]:
+            while sequence[offset - mod] == comp[sequence[offset + mod + 1]]:
                 mod += 1
                 if mod * 2 >= min_pal:
                     yield offset - mod + 1, mod * 2
@@ -76,18 +80,29 @@ def palindromes_by_nuclei(sequence, min_pal=4, max_pal=12):
             pass
 
 
-def main():
+def main(max_pal, min_pal):
+    if max_pal is not None:
+        max_pal = int(max_pal)
+    if min_pal is not None:
+        min_pal = int(min_pal)
+
     name, sequence = parse_fasta_sequence(arguments['<input>'])
-    for offset, length in palindromes_by_nuclei(sequence):
+
+    for offset, length in palindromes_by_nuclei(sequence, max_pal, min_pal):
         print('{0} {1}'.format(offset + 1, length))
 
 
-def compare():
+def compare(max_pal, min_pal):
+    if max_pal is not None:
+        max_pal = int(max_pal)
+    if min_pal is not None:
+        min_pal = int(min_pal)
+
     name, sequence = parse_fasta_sequence(arguments['<input>'])
 
     start = time()
     for i in range(100):
-        for offset, length in palindromes_by_nuclei(sequence):
+        for offset, length in palindromes_by_nuclei(sequence, max_pal, min_pal):
             pass
     print('''It took {0} seconds to complete 100 iterations of the Palindrome
 by Nuclei search.\n'''.format(time() - start))
@@ -96,6 +111,6 @@ by Nuclei search.\n'''.format(time() - start))
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.0.1')
     if arguments['--compare']:
-        compare()
+        compare(arguments['--max'], arguments['--min'])
     else:
-        main()
+        main(arguments['--max'], arguments['--min'])
