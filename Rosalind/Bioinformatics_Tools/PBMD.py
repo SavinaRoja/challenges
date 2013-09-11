63a7faa67bf4b3b0fd8fa949f6447cb161593d65
Paul Barton <pablo.barton@gmail.com>
2013-09-11 14:27:55 -0400

    More or less done with PBMD

--- Rosalind/Bioinformatics_Tools/PBMD.py
+++ Rosalind/Bioinformatics_Tools/PBMD.py
@@ -97,7 +97,7 @@ def get_abbrev_journal_name(full_journal_name):
 
 def get_earliest_record(records):
     '''
-    Looks for the  namedtuple(pmid, pubdate, abbrev_journal_name) in a list with
+    Looks for the namedtuple(pmid, pubdate, abbrev_journal_name) in a list with
     the following attributes, in order or priority:
       Lowest PubDate
       Highest alphabetical Abbreviated Journal Name
@@ -106,10 +106,10 @@ def get_earliest_record(records):
     '''
     earliest = item_info(None, date.today(), None)
     for record in records:
-        if record.pubdate < earliest.pubdate:  # Date order sorting
+        if record.pubdate < earliest.pubdate:  # Date order priority
             earliest = record
         elif record.pubdate == earliest.pubdate:
-            #Journal name resolves ambiguity in sorting equivalent dates
+            #Journal name resolves ambiguity in equivalent dates
             if record.abbrev_journal_name > earliest.abbrev_journal_name:
                 earliest = record
             elif record.abbrev_journal_name == earliest.abbrev_journal_name:
@@ -147,8 +147,8 @@ def main():
         abbrev_journal_name = get_abbrev_journal_name(full_journal_name)
         records.append(item_info(pmid, pubdate, abbrev_journal_name))
 
-    #Now we can do search, first by date, then by abbreviated journal name
-    print('Finding earliest result by: date, abbrev. journal name, PMID')
+    #Now we can do search, by date, abbreviated journal name, then PMID
+    print('Finding earliest result by: date, abbrev. journal name, PMID...')
     earliest = get_earliest_record(records)
 
     #Finally we can print our earliest publication
