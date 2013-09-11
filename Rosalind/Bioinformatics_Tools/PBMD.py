814d2acbf97665b5aa1e01fbd37cc5cf87e1cf38
Paul Barton <pablo.barton@gmail.com>
2013-09-11 13:52:17 -0400

    Working on polished solution of PBMD

--- Rosalind/Bioinformatics_Tools/PBMD.py
+++ Rosalind/Bioinformatics_Tools/PBMD.py
@@ -48,8 +48,11 @@ Sample Output
 from docopt import docopt
 from Bio import Entrez
 from datetime import date
+from collections import namedtuple
 
 SEARCH_FORM = '"{0}"[AU] AND ("{1}/1/1"[DP] : "{1}/12/31"[DP])'
+
+item_info = namedtuple('ItemInfo', 'pmid, pubdate, abbrev_journal_name')
 
 
 def get_search_parameters(inp_file):
@@ -62,9 +65,10 @@ def get_search_parameters(inp_file):
 def parse_pubdate_string_to_ints(pubdate):
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
               'Oct', 'Nov', 'Dec']
+    seasons = {'Spring': 3, 'Summer': 6, 'Fall': 9, 'Winter': 12}
     #There should always be a year
     year, others = pubdate.split(' ')[0], pubdate.split(' ')[1:]
-    year = int(year)
+    year = int(year.split('-')[0])
     #If the month or day do not exist, they will be 1 for our purposes
     #If we are given an interval, "May-Jun" or "12-17", the first will be used
     try:
@@ -73,7 +77,6 @@ def parse_pubdate_string_to_ints(pubdate):
         month = 1
         day = 1
     except ValueError:
-        seasons = {'Spring': 2,'Summer': 5, 'Fall':  8, 'Winter': 11}
         month = seasons[others[0]]
         day = 1
     else:
@@ -84,26 +87,72 @@ def parse_pubdate_string_to_ints(pubdate):
     return year, month, day
 
 
+def get_abbrev_journal_name(full_journal_name):
+    handle = Entrez.esearch(db='journals', term=full_journal_name)
+    record = Entrez.read(handle)
+    summ_handle = Entrez.esummary(db='journals', id=record['IdList'][0])
+    summ_record = Entrez.read(summ_handle)
+    return summ_record[0]['MedAbbr']
+
+
+def get_earliest_record(records):
+    '''
+    Looks for the  namedtuple(pmid, pubdate, abbrev_journal_name) in a list with
+    the following attributes, in order or priority:
+      Lowest PubDate
+      Highest alphabetical Abbreviated Journal Name
+      Smallest PMID
+    This record is then returned
+    '''
+    earliest = item_info(None, date.today(), None)
+    for record in records:
+        if record.pubdate < earliest.pubdate:  # Date order sorting
+            earliest = record
+        elif record.pubdate == earliest.pubdate:
+            #Journal name resolves ambiguity in sorting equivalent dates
+            if record.abbrev_journal_name > earliest.abbrev_journal_name:
+                earliest = record
+            elif record.abbrev_journal_name == earliest.abbrev_journal_name:
+                #Resolving beyond this is undocumented by PubMed, but I observed
+                #it with '"Lip GY"[AU] and "2012"[DP]', I used PMID to resolve
+                #this last level of ambiguity, in accordance with observed datum
+                if record.pmid < earliest.pmid:
+                    earliest = record
+    return earliest
+
+
 def main():
     name, year = get_search_parameters(arguments['<input>'])
     Entrez.email = arguments['<email>']
-    handle = Entrez.esearch(db='pubmed', term=SEARCH_FORM.format(name, year),
-                            retmax=500)
+
+    search = SEARCH_FORM.format(name, year)
+    print('Conducting search of PubMed... {0}'.format(search))
+    handle = Entrez.esearch(db='pubmed', term=search, retmax=500)
     record = Entrez.read(handle)
-    #As far as I know, there is no way to have esearch return a sorted list
-    #Some documentation may be read here:
-    #  http://www.ncbi.nlm.nih.gov/books/NBK25499/
-    #So I iterate through the UIDs by summary and look for the earliest date
-    earliest_date = (date.today(), None)
-    for uid in record['IdList']:
-        record = Entrez.read(Entrez.esummary(db='pubmed', id=uid))
-        pubdate_str = record[0]['PubDate']
-        year, month, day = parse_pubdate_string_to_ints(pubdate_str)
+
+    #Get summaries for the results, using a list is nicer to the server
+    print('Requesting summaries of matches...')
+    uids = ','.join(record['IdList'])  # must be comma-separated vals, not list
+    summary_handle = Entrez.esummary(db='pubmed', id=uids)
+    summary_record = Entrez.read(summary_handle)
+
+    #Extract the needed information for sorting and identification
+    print('Parsing matches for dates, retrieving abbreviated journal names...')
+    records = []
+    for item in summary_record:
+        pmid = item['ArticleIds']['pubmed'][0]
+        year, month, day = parse_pubdate_string_to_ints(item['PubDate'])
         pubdate = date(year, month, day)
-        print(uid, pubdate)
-        if pubdate < earliest_date[0]:
-            earliest_date = (pubdate, uid)
-    print(earliest_date[1])
+        full_journal_name = item['FullJournalName']
+        abbrev_journal_name = get_abbrev_journal_name(full_journal_name)
+        records.append(item_info(pmid, pubdate, abbrev_journal_name))
+
+    #Now we can do search, first by date, then by abbreviated journal name
+    print('Finding earliest result by: date, abbrev. journal name, PMID')
+    earliest = get_earliest_record(records)
+
+    #Finally we can print our earliest publication
+    print('PMID of earliest publication: {0}'.format(earliest.pmid))
 
 
 if __name__ == '__main__':
